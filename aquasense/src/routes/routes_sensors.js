d407644860a30e0e5a1d8a1dbc104b15076083e8
csaranja <69058049+csaranja@users.noreply.github.com>
2026-02-27 22:51:29 +0300

    fix: add getSensorById to sensorController and routes

--- aquasense/src/routes/routes_sensors.js
+++ aquasense/src/routes/routes_sensors.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const sensorController = require('../controllers/sensorController');
 const { protect } = require('../middleware/authMiddleware');
 
-router.post('/register', protect, sensorController.registerSensor);
-router.get('/my-sensors', protect, sensorController.getMySensors);
-router.get('/analytics/:sensorId', protect, sensorController.getSensorAnalytics);
-router.get('/:sensorId', protect, sensorController.getSensorById);  // ← NEW
+router.post('/register',                   protect, sensorController.registerSensor);
+router.get('/my-sensors',                  protect, sensorController.getMySensors);
+router.get('/analytics/:sensorId',         protect, sensorController.getSensorAnalytics);
+router.get('/:sensorId',                   protect, sensorController.getSensorById);
 
 module.exports = router;
